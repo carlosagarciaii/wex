be48f186367318ca9467ce8238968b935bc35952
Carlos Garcia II <carlosagarciaii@gmail.com>
2026-05-16 13:12:30 -0500

    Adding DB Service

---
+++ source/TransactAPI/DatabaseService/DBFlavors/MariaDBConn.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace DatabaseService.DBFlavors;
+
+public class MariaDBConn: IAsyncDisposable
+{
+
+    #region Properties
+
+    private int connections = 0;
+
+    #endregion
+
+
+    #region LifeCycle
+        private static SemaphoreSlim _lockObj = new(1, 1);
+    private static List<MariaDBConn> _instances { get; set; } = new();
+    public ReadOnlyCollection<MariaDBConn> Instances { get => _instances.AsReadOnly(); }
+
+
+
+    public async ValueTask DisposeAsync()
+    {
+
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    #endregion
+
+
+
+}
